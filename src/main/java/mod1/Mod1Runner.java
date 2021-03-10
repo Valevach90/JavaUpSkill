71b59026e4c567dd67659d50f6bbc3fb4281b166
Valevach.90 <Valevach.90@mail.ru>
2021-03-10 18:25:17 +0300

    second commit

--- src/main/java/mod1/Mod1Runner.java
+++ src/main/java/mod1/Mod1Runner.java
@@ -2,6 +2,6 @@ package mod1;
 
 public class Mod1Runner {
     public static void main(String[] args) {
-        System.out.println("Hello,friend");
+        System.out.println("Hello,to everybody");
     }
 }
