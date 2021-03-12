88b54d006d44c38dab1df3d02d567463523d3bee
Valevach.90 <Valevach.90@mail.ru>
2021-03-12 20:21:37 +0300

    пробная программа вер2

--- mod2/src/main/java/ParserJson.java
+++ mod2/src/main/java/ParserJson.java
@@ -7,6 +7,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParserJson {
     private static final String TAG_NAME_MAIN = "name";
@@ -24,15 +26,16 @@ public class ParserJson {
 
             JSONArray peopleArray = (JSONArray) jObject.get(TAG_PEOPLE);
             String newName = null;
+            List<People> peoples = new ArrayList<>();
             for (Object people : peopleArray) {
                 JSONObject peopleToJSONObject = (JSONObject) people;
                 String peopleName = (String) peopleArray.get(Integer.parseInt((TAG_NAME)));
                 newName = suffix + peopleName;
+                int age = (Integer) jObject.get(TAG_AGE);
+                People newPeople = new People(newName, age);
+                peoples.add(newPeople);
             }
-
-            root.setName(newName);
-
-
+            root.setPeople(peoples);
         } catch (IOException e) {
             e.printStackTrace();
         }
